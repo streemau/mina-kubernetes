c8f50bca5e3aafc134b3983b4ab5a504c0f5de33
Tim Flapper <tim.flapper@streem.com.au>
2020-07-22 17:01:16 +1000

    Add HTTPS_PROXY env variable when proxy is set

--- lib/mina/kubernetes.rb
+++ lib/mina/kubernetes.rb
@@ -8,6 +8,7 @@ require "base64"
 set :execution_mode, :pretty
 
 namespace :kubernetes do
+  set :proxy, nil
 
   task :deploy, [:options] do |task, args|
     desc "Set image tag to be latest commit of prompted branch (unless provided) then applies resources to cluster"
@@ -45,6 +46,21 @@ end
 
 private
 
+def kubectl_command(args)
+  proxy_env = "HTTPS_PROXY=#{fetch(:proxy)}" if fetch(:proxy)
+  command "#{proxy_env} kubectl #{args}"
+end
+
+def kubectl_system(args)
+  proxy_env = "HTTPS_PROXY=#{fetch(:proxy)}" if fetch(:proxy)
+  system "#{proxy_env} kubectl #{args}"
+end
+
+def krane_command(args)
+  proxy_env = "HTTPS_PROXY=#{fetch(:proxy)}" if fetch(:proxy)
+  command "#{proxy_env} krane #{args}"
+end
+
 def env_hash_arg
   @env_hash_arg ||= (fetch(:env_hash).is_a?(String) ? JSON.parse(fetch(:env_hash)) : fetch(:env_hash)) || {}
 end
@@ -61,7 +77,7 @@ end
 def create_namespace_on_cluster
   run :local do
     comment "Create/update namespace on Kubernetes cluster..."
-    command "kubectl create namespace #{fetch(:namespace)} --dry-run -o yaml | kubectl apply -f - --context=#{fetch(:kubernetes_context)}"
+    kubectl_command "create namespace #{fetch(:namespace)} --dry-run -o yaml | kubectl apply -f - --context=#{fetch(:kubernetes_context)}"
   end
 end
 
@@ -84,17 +100,18 @@ end
 def run_command(command, env_hash = {})
   env = env_hash.collect{|k,v| "--env #{k}=#{v}" }.join(" ")
   label = command.downcase.gsub(" ", "-").gsub(":", "-")
+  proxy_env = "HTTPS_PROXY=#{fetch(:proxy)}" if fetch(:proxy)
   # using system instead of mina's command so tty opens successfully
-  system "kubectl run #{label}-#{SecureRandom.hex(4)} --rm -i --tty --restart=Never --context=#{fetch(:kubernetes_context)} --namespace=#{fetch(:namespace)} --image #{fetch(:image_repo)}:#{fetch(:image_tag)} #{env} -- #{command}"
+  kubectl_system "run #{label}-#{SecureRandom.hex(4)} --rm -i --tty --restart=Never --context=#{fetch(:kubernetes_context)} --namespace=#{fetch(:namespace)} --image #{fetch(:image_repo)}:#{fetch(:image_tag)} #{env} -- #{command}"
 end
 
 def apply_kubernetes_resources(options)
   run :local do
     comment "Apply all Kubernetes resources..."
     filepaths = options&.[](:filepaths) || "config/deploy/#{fetch(:stage)}"
-    render_cmd = "krane render --bindings=image_repo=#{fetch(:image_repo)},image_tag=#{fetch(:image_tag)},namespace=#{fetch(:namespace)} --current_sha #{fetch(:image_tag)} -f #{filepaths}"
-    deploy_cmd = "krane deploy #{fetch(:namespace)} #{fetch(:kubernetes_context)} --stdin "
+    render_cmd = "render --bindings=image_repo=#{fetch(:image_repo)},image_tag=#{fetch(:image_tag)},namespace=#{fetch(:namespace)} --current_sha #{fetch(:image_tag)} -f #{filepaths}"
+    deploy_cmd = "deploy #{fetch(:namespace)} #{fetch(:kubernetes_context)} --stdin "
     deploy_cmd += options[:deployment_options] if options&.[](:deployment_options)
-    command "#{render_cmd} | #{deploy_cmd}"
+    krane_command "#{render_cmd} | #{deploy_cmd}"
   end
 end
